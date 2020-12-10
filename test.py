8b192e5bec59315fdb6ec1b42ae2f51c80b59505
Benjamin Leloup <benjamin.leloup@epita.fr>
2020-12-10 23:46:56 +0100

    add logic

--- test.py
+++ test.py
@@ -1,10 +1,44 @@
-from gpiozero import LED
-from time import sleep
+import requests
+from pynput.keyboard import Key, Listener
 
-led = LED(17)
+url = "http://127.0.0.1:8000/entry/"
+mode = True
+cap1 = True
+cap2 = True
+crossing = False
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+def on_press(key):
+    None
+
+def on_release(key):
+    try:
+        global cap1
+        global cap2
+        global mode
+        global crossing
+        if key.char == "s":
+            cap1 = not cap1
+            if mode and not cap1 and not crossing:
+                crossing = True
+            if not mode and cap1 and crossing:
+                crossing = False
+                requests.post(url, json={"isEntry": False})
+        elif key.char == "e":
+            cap2 = not cap2
+            if not mode and not cap2 and not crossing:
+                crossing = True
+            if mode and cap2 and crossing:
+                crossing = False
+                requests.post(url, json={"isEntry": True})
+        elif key.char == "m":
+            mode = not mode
+            crossing = False
+    except AttributeError:
+        if key == Key.esc:
+            return False
+
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
